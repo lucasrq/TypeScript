bbcb18756cafb1167e50b72a230f815a3f934f26
lucasrq <lucasroque545@gmail.com>
2024-02-28 20:21:56 -0300

    new

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(n1, n2) {
+    return n1 * n2;
+}
+console.log(multiplicacao(2, 3));
+function saudacao(nome) {
+    return `ola ${nome} Como voce esta?`;
+}
+console.log(saudacao("lucas"));
---
+++ main.ts
@@ -0,0 +1,11 @@
+function multiplicacao(n1: number, n2: number): number {
+    return n1 * n2
+}
+
+console.log(multiplicacao(2, 3))
+
+function saudacao(nome: string): string {
+    return `ola ${nome} Como voce esta?`
+}
+
+console.log(saudacao("lucas"))
